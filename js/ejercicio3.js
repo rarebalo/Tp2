cb3a0c0406741ee6491a42c248c6505e12ce5409
Roberto <roberto.v.arebalo@gmail.com>
2023-11-01 23:28:56 -0300

    Se carga el ejercicio 3 terminado

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -3,3 +3,13 @@
 
 Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp */
 
+let palabrasUsuario = [];
+let cadenaFinal;
+
+do {
+  palabrasUsuario.push(prompt("Ingresa una cadena de texto:"));
+} while (confirm("Ingresas otra palabra?"));
+
+cadenaFinal = palabrasUsuario.join("-");
+
+alert(cadenaFinal);
