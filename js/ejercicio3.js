9e5f8b00dfe27ec36d39466994534347f032f54d
Roberto <roberto.v.arebalo@gmail.com>
2023-11-01 23:09:03 -0300

    Se crea lo necesario para trabajar en el ejercicio3

---
+++ js/ejercicio3.js
@@ -0,0 +1,5 @@
+/*3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”.
+ Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp */
+
