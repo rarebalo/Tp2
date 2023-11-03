69c7e67e42499208d0ea2ae5701da2ac68adb4bf
Roberto <roberto.v.arebalo@gmail.com>
2023-11-03 20:21:41 -0300

    Agrego el ejercicio 7 terminado

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -10,3 +10,19 @@ al número que indique el usuario (no mayor de 50)  de la siguiente forma : (sup
 22
 1
 */
+let  numeroUsuario = 0;
+
+do {
+  numeroUsuario = parseInt(
+    prompt("Ingrese un numero (no mayor a 50) para formar una priramide inversa: ")
+  );
+} while (numeroUsuario > 50);
+
+
+for (let piso = numeroUsuario; piso > 0; piso--) {
+  for (let index = 0; index < piso; index++) {
+    document.write(piso);
+  }
+
+  document.write("<br>");
+}
