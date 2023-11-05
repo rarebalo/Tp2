7e66b9103e120db6294534f9ed895e2d197df7ac
Roberto <roberto.v.arebalo@gmail.com>
2023-11-05 11:43:58 -0300

    Agrego el ejercicio16 terminado

--- js/ejercicio16.js
+++ js/ejercicio16.js
@@ -1,3 +1,15 @@
 //16- Realiza un script que pida una cadena de texto y la devuelva al revés.
 // Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
 
+const textoUsuario = prompt("Ingrese el texto que vamos a licuar");
+
+const preparandoTextoParaElLicuado = textoUsuario.split("");
+let textoLicuado = [];
+
+
+for (let index =  preparandoTextoParaElLicuado.length -1; index >= 0; index--) {
+   textoLicuado.push(preparandoTextoParaElLicuado[index]);      
+}
+
+
+alert(textoLicuado.join(""));
