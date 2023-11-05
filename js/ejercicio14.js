7908d2a24315cbdf0d968794447325e49b8523fa
Roberto <roberto.v.arebalo@gmail.com>
2023-11-05 10:37:34 -0300

    Se carga el ejercicio14 terminado

---
+++ js/ejercicio14.js
@@ -0,0 +1,12 @@
+/*
+14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre 
+cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”. */
+
+let textoUsuario = prompt(
+  "Ingrese el texto al que se le aplicara la magia: "
+);
+
+textoUsuario = textoUsuario.split("");
+textoUsuario = textoUsuario.join("-");
+
+alert(textoUsuario);
