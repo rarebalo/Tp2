8dddafadc40bceaff09f632e111f28c2f85c5bdf
Roberto <roberto.v.arebalo@gmail.com>
2023-11-06 08:01:01 -0300

    Se agrega el ejercicio extra terminado

--- js/ejercicioExtra.js
+++ js/ejercicioExtra.js
@@ -1,17 +1,36 @@
-const opcion = prompt(
-  "Seleccione una opcion: 1- Consulte el saldo, 2- Ingrese dinero, 3-Extraiga dinero"
-);
-
 let saldo = 0;
+let saldoIngresado = 0;
+let volverAOperar = 2;
 
-switch (opcion) {
-  case value:
-    break;
-  case value:
-    break;
-  case value:
-    break;
+do {
+  const opcion = parseInt(
+    prompt(
+      "Seleccione una opcion: 1- Consulte el saldo, 2- Ingrese dinero, 3-Extraiga dinero"
+    )
+  );
 
-  default:
-    break;
-}
+  switch (opcion) {
+    case 1:
+      alert(`Su saldo es: ${saldo}`);
+      break;
+    case 2:
+      saldoIngresado = parseInt(prompt("Ingrese el importe:"));
+
+      saldo = +saldoIngresado;
+      alert(`Su saldo actual es ${saldo}`);
+      break;
+    case 3:
+      saldoIngresado = parseInt(prompt("Ingrese el importe:"));
+
+      saldo = -saldoIngresado;
+      alert(`Su saldo actual es ${saldo}`);
+      break;
+
+    default:
+      alert("La opcion ingresada es incorrecta, ingrese una opcion correcta");
+      break;
+  }
+
+  volverAOperar = parseInt(prompt("Desea realizar otra operacion? 1- SI 2- NO"));
+  
+} while (volverAOperar === 1);
