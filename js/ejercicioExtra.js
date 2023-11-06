2f126e2cf877d845d2623d7f76f7bffb25c12fc0
Roberto <roberto.v.arebalo@gmail.com>
2023-11-06 07:41:26 -0300

    Se trabaja de forma erronea en la rama dev, se hace un commit para crear la rama correspondiente sin tener conflicto

---
+++ js/ejercicioExtra.js
@@ -0,0 +1,17 @@
+const opcion = prompt(
+  "Seleccione una opcion: 1- Consulte el saldo, 2- Ingrese dinero, 3-Extraiga dinero"
+);
+
+let saldo = 0;
+
+switch (opcion) {
+  case value:
+    break;
+  case value:
+    break;
+  case value:
+    break;
+
+  default:
+    break;
+}
